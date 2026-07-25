96dc065743c8e8a46a2f97b7f0bc7deb4296db56
Prasanth B <41023256+kbprashant@users.noreply.github.com>
2026-07-25 11:18:10 +0000

    first commit

---
+++ 3536.Maximum Product of Two Digits.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProduct(int n) {
+        int max1=0;
+        int max2=0;
+
+        while(n>0){
+            int last=n%10;
+            if(last>max1){
+                max2=max1;
+                max1=last;
+            }
+            else if(last>max2){
+                max2=last;
+            }
+            n/=10;
+        }
+
+        return max1*max2;
+        
+    }
+}
