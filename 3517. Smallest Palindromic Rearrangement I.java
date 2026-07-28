599045168593688b677f41dcd8e889265c1c41d3
Prasanth B <41023256+kbprashant@users.noreply.github.com>
2026-07-28 06:20:53 +0000

    3517. Smallest Palindromic Rearrangement I

---
+++ 3517. Smallest Palindromic Rearrangement I.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String smallestPalindrome(String s) {
+        int partition=s.length()/2;
+        int[] bucket= new int[26];
+
+        for(int i =0;i<partition;i++){
+            bucket[s.charAt(i)-'a']+=1;
+        }
+
+        StringBuilder left=new StringBuilder();
+
+        for(int i=0;i<26;i++){
+            if(bucket[i]>0){
+                left.append(String.valueOf((char)(i+'a')).repeat(bucket[i]));
+            }
+        }
+
+        String mid;
+        if(s.length()%2==0)
+        mid="";
+        else
+        mid=String.valueOf(s.charAt(partition));
+
+        String right=new StringBuilder(left).reverse().toString();
+
+        return left.toString()+mid+right;
+    }
+}
